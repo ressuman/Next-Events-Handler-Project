c9a8a571526f101003997ea811a829214d96dfdd
Richard Essuman <ressuman001@gmail.com>
2024-09-07 15:10:04 +0000

    "Added new product to dummy-backend.json and updated Home component in index.js to dynamically render products from JSON data"

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,33 @@
-export default function Home() {
+import path from "path";
+import fs from "fs/promises";
+
+export default function Home({ products }) {
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((product) => (
+        <li key={product.id}>
+          {product.title}- &nbsp;
+          <span>{product.description}</span>
+        </li>
+      ))}
     </ul>
   );
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return {
+    props: {
+      products: [
+        {
+          id: "p1",
+          title: "Product 1",
+          description: "This is product 1",
+        },
+      ],
+    },
+  };
 }
