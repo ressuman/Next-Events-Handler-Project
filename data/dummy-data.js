8a51eb595475eed2268b1f5cc4ef22e00df31657
Richard Essuman <ressuman001@gmail.com>
2024-09-09 21:06:54 +0000

    "Added getFeaturedEvents function to dummy-data.js and getEventById function to api-utils.js"

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -81,6 +81,10 @@ const DUMMY_EVENTS = [
   },
 ];
 
+export function getFeaturedEvents() {
+  return DUMMY_EVENTS.filter((event) => event.isFeatured);
+}
+
 export function getAllEvents() {
   return DUMMY_EVENTS;
 }
