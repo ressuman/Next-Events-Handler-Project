f6dcbf97eefc4e10e5b928fcd3d8eed63d3743be
Richard Essuman <ressuman001@gmail.com>
2024-09-10 19:55:19 +0000

    Refactor global styles and add layout wrapper component
    - Refactored global styles in `globals.css` to improve readability and maintainability.
    - Added a new layout wrapper component `RootLayout` in `_app.js` to provide a consistent layout for all pages.
    - Updated the `App` component to include the `RootLayout` and added meta tags for SEO optimization.
    - Removed the unused `Home.module.css` file.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,26 @@
 import "@/styles/globals.css";
+import RootLayout from "@/wrapper/layout/layout";
+import Head from "next/head";
+import PropTypes from "prop-types";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <RootLayout>
+      <Head>
+        <title>Ressuman Events Handler</title>
+        <meta
+          name="description"
+          content="Everything you need to know about scheduling an event"
+        />
+        <meta name="viewport" content="initial-scale=1.0,width=device-width" />
+        <link rel="icon" type="image/svg" href="/icon.svg" />
+      </Head>
+      <Component {...pageProps} />
+    </RootLayout>
+  );
 }
+
+App.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object,
+};
