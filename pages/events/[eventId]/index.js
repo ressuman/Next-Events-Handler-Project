a99abc327d5f226970d97388684cfcf45b2ea2ea
Richard Essuman <ressuman001@gmail.com>
2024-09-10 19:59:08 +0000

    Refactor filtered events page, add dynamic filtering and error handling

---
+++ pages/events/[eventId]/index.js
@@ -0,0 +1,73 @@
+import EventContent from "@/components/event-content/event-content";
+import EventLogistics from "@/components/event-logistics/event-logistics";
+import EventSummary from "@/components/event-summary/event-summary";
+import {
+  getAllEvents,
+  getEventById,
+  getFeaturedEvents,
+} from "@/helpers/utils/api-utils";
+import Head from "next/head";
+
+export default function EventsDetailPage({ selectedEvent }) {
+  const event = selectedEvent;
+
+  if (!event) {
+    return (
+      <div className="center">
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <Head>
+        <title>All Events-{event.title}</title>
+        <meta name="description" content={event.description} />
+        <link rel="icon" type="image/svg" href="/icon.svg" />
+      </Head>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  );
+}
+
+export async function getStaticProps({ params }) {
+  //const eventId = ctx.params.eventId;
+  const { eventId } = params;
+
+  const event = await getEventById(eventId);
+
+  return {
+    props: {
+      selectedEvent: event,
+    },
+    revalidate: 10,
+  };
+}
+
+export async function getStaticPaths() {
+  //const events = await getAllEvents();
+  const events = await getFeaturedEvents();
+
+  const paths = events.map((event) => ({
+    params: {
+      eventId: event.id,
+    },
+  }));
+
+  return {
+    paths: paths,
+    //fallback: false,
+    //fallback: true,
+    fallback: "blocking",
+  };
+}
---
+++ pages/events/index.js
@@ -0,0 +1,39 @@
+import EventList from "@/components/event-list/event-list";
+import EventSearch from "@/components/event-search/event-search";
+import { getAllEvents } from "@/helpers/utils/api-utils";
+import Head from "next/head";
+
+import { useRouter } from "next/router";
+
+export default function AllEventsPage({ events }) {
+  const router = useRouter();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Ressuman-All Events</title>
+        <meta name="description" content="Find all events we cater for" />
+        <link rel="icon" type="image/svg" href="/icon.svg" />
+      </Head>
+      <EventSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events,
+    },
+    revalidate: 60,
+  };
+}
