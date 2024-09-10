0d525e63b985be11db24dc80308bb7192766deea
Richard Essuman <ressuman001@gmail.com>
2024-09-10 17:55:15 +0000

    "Comment out div with id 'overlays' in _document.js"

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
-        <div id="overlays" />
+        {/* <div id="overlays" /> */}
         <Main />
         <NextScript />
       </body>
