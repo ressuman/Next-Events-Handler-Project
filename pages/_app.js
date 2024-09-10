f8258895f3cff9270501ea1c2ed75a59be1a1c8b
Richard Essuman <ressuman001@gmail.com>
2024-09-10 10:49:04 +0000

    "Added Head component with meta viewport tag to _app.js"

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import "@/styles/globals.css";
 import RootLayout from "@/wrapper/layout/layout";
+import Head from "next/head";
 import PropTypes from "prop-types";
 
 export default function App({ Component, pageProps }) {
   return (
     <RootLayout>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0,width=device-width" />
+      </Head>
       <Component {...pageProps} />
     </RootLayout>
   );
