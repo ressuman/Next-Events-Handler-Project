edbea51597db1647a6ec536097b69953e2fcbce8
Richard Essuman <ressuman001@gmail.com>
2024-09-10 20:00:48 +0000

    Refactor API utility functions for fetching and filtering events

---
+++ helpers/utils/api-utils.js
@@ -0,0 +1,58 @@
+export async function getAllEvents() {
+  const url = process.env.NEXT_PUBLIC_FB_RTDB_REF_URL;
+
+  const response = await fetch(`${url}/events.json`);
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const data = await response.json();
+
+  const transformedEventsIntoArray = [];
+
+  for (const key in data) {
+    // transformedEventsIntoArray.push({
+    //   id: key,
+    //   title: data[key].title,
+    //   description: data[key].description,
+    //   location: data[key].location,
+    //   date: data[key].date,
+    //   image: data[key].image,
+    //   isFeatured: data[key].isFeatured,
+    // });
+    transformedEventsIntoArray.push({
+      id: key,
+      ...data[key],
+    });
+  }
+
+  return transformedEventsIntoArray;
+}
+
+export async function getFeaturedEvents() {
+  const allEvents = await getAllEvents();
+
+  return allEvents.filter((event) => event.isFeatured);
+}
+
+export async function getEventById(id) {
+  const allEvents = await getAllEvents();
+
+  return allEvents.find((event) => event.id === id);
+}
+
+export async function getFilteredEvents(dateFilter) {
+  const { year, month } = dateFilter;
+
+  const allEvents = await getAllEvents();
+
+  let filteredEvents = allEvents.filter((event) => {
+    const eventDate = new Date(event.date);
+    return (
+      eventDate.getFullYear() === year && eventDate.getMonth() === month - 1
+    );
+  });
+
+  return filteredEvents;
+}
