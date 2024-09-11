1a4c76f2eb052b17f73b1529a1ad3620e3287556
Richard Essuman <ressuman001@gmail.com>
2024-09-11 20:07:28 +0000

    "Updated hello.js API route and added form with input fields and submit handler to index.js"

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,36 @@
+import { useRef } from "react";
+
 function HomePage() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" name="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea
+            id="feedback"
+            cols="30"
+            rows="10"
+            name="feedback"
+            ref={feedbackInputRef}
+          ></textarea>
+        </div>
+        <button type="submit">Send Feedback</button>
+      </form>
     </div>
   );
 }
