afdc9a11ae1ba4a70a9384f4556a638f99b46635
Richard Essuman <ressuman001@gmail.com>
2024-09-12 08:51:32 +0000

    "Added feedback submission functionality to HomePage and initialized feedbackDb.json with two entries"

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,22 @@ function HomePage() {
 
     const enteredEmail = emailInputRef.current.value;
     const enteredFeedback = feedbackInputRef.current.value;
+
+    const reqBody = {
+      email: enteredEmail,
+      text: enteredFeedback,
+    };
+
+    fetch("/api/feedback", {
+      method: "POST",
+      body: JSON.stringify(reqBody),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error));
   }
 
   return (
