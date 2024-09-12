8ed4378f58355958b805fba77c79a55af7ae9a04
Richard Essuman <ressuman001@gmail.com>
2024-09-12 07:46:07 +0000

    "Updated feedback API to handle POST requests and store feedback data in a JSON file"

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,6 +1,34 @@
+import fs from "fs";
+import path from "path";
+
 export default function handler(req, res) {
-  res.status(200).json({
-    name: "John Doe",
-    message: "This works!.",
-  });
+  if (req.method === "POST") {
+    const { email, text } = req.body;
+
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email,
+      text,
+    };
+
+    //store the new feedback in a database or in a file
+    const filePath = path.join(process.cwd(), "data", "feedbackDb.json");
+
+    const fileData = fs.readFileSync(filePath);
+
+    const data = JSON.parse(fileData);
+    data.push(newFeedback);
+
+    fs.writeFileSync(filePath, JSON.stringify(data));
+
+    res.status(201).json({
+      message: "Success",
+      feedback: newFeedback,
+    });
+  } else {
+    res.status(200).json({
+      name: "John Doe",
+      message: "This works!.",
+    });
+  }
 }
