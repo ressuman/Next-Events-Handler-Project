feb0a92a50c54321954e15deb9ac399fdd683e21
Richard Essuman <ressuman001@gmail.com>
2024-09-13 22:01:12 +0000

    "Added email input validation and API call to newsletter registration component"

--- components/input/newsletter-registration/newsletter-registration.js
+++ components/input/newsletter-registration/newsletter-registration.js
@@ -1,12 +1,42 @@
 import styles from "./newsletter-registration.module.css";
+import { useRef, useState } from "react";
 
 export default function NewsletterRegistration() {
+  const [isInvalid, setIsInvalid] = useState(false);
+
+  const emailInputRef = useRef();
+
   function registrationHandler(event) {
     event.preventDefault();
 
-    // fetch user input (state or refs)
-    // optional: validate input
-    // send valid data to API
+    const enteredEmail = emailInputRef.current.value;
+
+    if (
+      !enteredEmail ||
+      enteredEmail.trim() === "" ||
+      !enteredEmail.includes("@")
+    ) {
+      setIsInvalid(true);
+      return;
+    }
+
+    const reqBody = {
+      email: enteredEmail,
+    };
+
+    fetch("/api/newsletter", {
+      method: "POST",
+      body: JSON.stringify(reqBody),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // setFeedbackData(data.feedback);
+        console.log(data);
+      })
+      .catch((error) => console.log(error));
   }
 
   return (
@@ -19,6 +49,7 @@ export default function NewsletterRegistration() {
             id="email"
             placeholder="Your email"
             aria-label="Your email"
+            ref={emailInputRef}
           />
           <button>Register</button>
         </div>
---
+++ pages/api/newsletter/index.js
@@ -0,0 +1,19 @@
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    const { email } = req.body;
+
+    if (!email || !email.includes("@")) {
+      res.status(422).json({
+        message: "Invalid email address",
+      });
+
+      return;
+    }
+
+    console.log(email);
+
+    res.status(201).json({
+      message: "Signed Up!.",
+    });
+  }
+}
