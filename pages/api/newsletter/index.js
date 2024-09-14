a2462a719b067b6689b4d9c3631ef5de63c7511d
Richard Essuman <ressuman001@gmail.com>
2024-09-14 13:24:08 +0000

    Refactor newsletter API handler to use MongoDB for database operations

--- pages/api/newsletter/index.js
+++ pages/api/newsletter/index.js
@@ -1,5 +1,31 @@
 import { MongoClient } from "mongodb";
 
+async function connectDatabase() {
+  const url = process.env.MONGO_DB_URL;
+  const client = new MongoClient(url);
+  const dbName = "Events-Newsletter";
+
+  try {
+    await client.connect();
+    return { client, dbName };
+  } catch (error) {
+    console.error("Database connection error:", error);
+    throw new Error("Failed to connect to the database.");
+  }
+}
+
+async function insertDocument(client, dbName, document) {
+  try {
+    const db = client.db(dbName);
+    const collection = db.collection("emails");
+    const insertResult = await collection.insertOne(document);
+    return insertResult;
+  } catch (error) {
+    console.error("Document insertion error:", error);
+    throw new Error("Failed to insert document.");
+  }
+}
+
 export default async function handler(req, res) {
   if (req.method === "POST") {
     const { email } = req.body;
@@ -8,31 +34,30 @@ export default async function handler(req, res) {
       return res.status(422).json({ message: "Invalid email address" });
     }
 
-    const newRegistration = {
-      email,
-    };
+    const newRegistration = { email };
 
-    const url = process.env.MONGO_DB_URL;
-    const client = new MongoClient(url);
-    const dbName = "Events-Newsletter";
-
+    let client;
     try {
-      await client.connect();
-      const db = client.db(dbName);
-      const collection = db.collection("emails");
+      const { client, dbName } = await connectDatabase(); // Database connection wrapped in try-catch
+      const insertResult = await insertDocument(
+        client,
+        dbName,
+        newRegistration
+      ); // Document insertion wrapped in try-catch
 
-      const insertResults = await collection.insertOne(newRegistration);
-      console.log("Inserted document:", insertResults);
+      console.log("Inserted document:", insertResult);
 
       res.status(201).json({
         message: "Signed Up!",
         data: newRegistration,
       });
     } catch (error) {
-      console.error("Database insertion error:", error);
-      res.status(500).json({ message: "Failed to sign up" });
+      console.error("Error:", error);
+      res.status(500).json({ message: error.message });
     } finally {
-      await client.close();
+      if (client) {
+        await client.close();
+      }
     }
   } else {
     res.status(405).json({ message: "Method not allowed" });
