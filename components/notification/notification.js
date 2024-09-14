489cfb5da276f4e07f89135a2ffe8e818402a357
Richard Essuman <ressuman001@gmail.com>
2024-09-14 19:09:20 +0000

    Refactor notification component and styles

---
+++ components/notification/notification.js
@@ -0,0 +1,33 @@
+//import { useContext } from "react";
+
+import styles from "./notification.module.css";
+//import NotificationContext from "../../store/notification-context";
+
+export default function Notification({ title, message, status }) {
+  //const notificationCtx = useContext(NotificationContext);
+
+  let statusClasses = "";
+
+  if (status === "success") {
+    statusClasses = styles.success;
+  }
+
+  if (status === "error") {
+    statusClasses = styles.error;
+  }
+
+  if (status === "pending") {
+    statusClasses = styles.pending;
+  }
+
+  const activeClasses = `${styles.notification} ${statusClasses}`;
+
+  return (
+    <div className={activeClasses}>
+      <h2>{title}</h2>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+//onClick={notificationCtx.hideNotification}
