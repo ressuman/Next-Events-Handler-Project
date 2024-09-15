f249e1ca3a1e808895bbd535115e6210c65c8a61
Richard Essuman <ressuman001@gmail.com>
2024-09-15 12:13:36 +0000

    "Added NotificationContext to Comments component, updated addCommentHandler to display notifications for pending, success, and error states"

--- components/input/comments/comments.js
+++ components/input/comments/comments.js
@@ -1,10 +1,13 @@
-import { useState, useEffect } from "react";
+import { useContext, useState, useEffect } from "react";
 
 import styles from "./comments.module.css";
 import NewComment from "../new-comment/new-comment";
 import CommentList from "../comment-list/comment-list";
+import NotificationContext from "@/store/notification-context";
 
 export default function Comments({ eventId }) {
+  const { showNotification } = useContext(NotificationContext);
+
   const [showComments, setShowComments] = useState(false);
   const [comments, setComments] = useState([]);
 
@@ -25,6 +28,13 @@ export default function Comments({ eventId }) {
   }
 
   function addCommentHandler(commentData) {
+    showNotification({
+      title: "Sending comment...",
+      message:
+        "Your comment is currently being processed and stored in a database.",
+      status: "pending",
+    });
+
     fetch(`/api/comments/${eventId}`, {
       method: "POST",
       body: JSON.stringify(commentData),
@@ -32,12 +42,29 @@ export default function Comments({ eventId }) {
         "Content-Type": "application/json",
       },
     })
-      .then((response) => response.json())
-      .then((data) => {
-        //setFeedbackData(data.feedback);
-        console.log(data);
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+
+        return response.json().then((data) => {
+          throw new Error(data.message || "Something went wrong!");
+        });
       })
-      .catch((error) => console.log(error));
+      .then((data) => {
+        showNotification({
+          title: "Success",
+          message: "Your comment has been processed and saved",
+          status: "success",
+        });
+      })
+      .catch((error) => {
+        showNotification({
+          title: "Error!",
+          message: error.message || "Something went wrong!",
+          status: "error",
+        });
+      });
   }
 
   return (
